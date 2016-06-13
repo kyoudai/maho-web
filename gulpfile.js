a03edf800b6ad035a0e2a0923dd4398c47d36a16
wanderer06 <georgica_fara_de_frica@yahoo.com>
2016-06-13 12:22:33 +0100

    Fix inconsistent quoting

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync').create(),
     less = require('gulp-less'),
     path = require('path'),
-    rename = require("gulp-rename");
+    rename = require('gulp-rename');
 
 gulp.task('less', function () {
   return gulp.src('source/less/theme.less')
@@ -13,10 +13,10 @@ gulp.task('less', function () {
 });
 
 gulp.task('watch', ['less'], function() {
-    browserSync.init({ server: "./" });
+    browserSync.init({ server: './' });
 
-    gulp.watch("source/less/*.less", ['less']);
-    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch('source/less/*.less', ['less']);
+    gulp.watch('./*.html').on('change', browserSync.reload);
 });
 
 gulp.task('default', ['watch']);
